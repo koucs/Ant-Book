fcc7d0cae072344509fe9c669f0028247aff8e44
koucs <kou.cshu@gmail.com>
2021-08-28 22:14:34 +0900

    ch2 (1)

---
+++ ch2/coin/main.py
@@ -0,0 +1,35 @@
+#! env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+# Ant-Book.main.py
+# Date: 2021/08/28
+# Filename: main.py 
+# Author: acttrd
+
+sys.setrecursionlimit(10 ** 8)
+INF = float('inf')
+mod = 10 ** 9 + 7
+eps = 10 ** -7
+AtoZ = [chr(i) for i in range(65, 65 + 26)]
+atoz = [chr(i) for i in range(97, 97 + 26)]
+input = sys.stdin.readline
+
+
+def main():
+    v = [1, 5, 10, 50, 100, 500]
+    c = list(map(int, input().split()))
+    a = int(input())
+
+    ans = 0
+    for i in range(5, -1, -1):
+        count_coin = min(a // v[i], c[i])
+        ans += count_coin
+        a -= v[i] * count_coin
+    print(ans)
+    return
+
+
+if __name__ == '__main__':
+    main()
---
+++ ch2/task_schedule/main.py
@@ -0,0 +1,42 @@
+#! env python
+# -*- coding: utf-8 -*-
+
+from collections import defaultdict
+import sys, heapq, bisect, math, itertools, string, queue, datetime, unittest
+
+# Ant-Book.main.py
+# Date: 2021/08/28
+# Filename: main.py 
+# Author: acttrd
+
+sys.setrecursionlimit(10 ** 8)
+INF = float('inf')
+mod = 10 ** 9 + 7
+eps = 10 ** -7
+AtoZ = [chr(i) for i in range(65, 65 + 26)]
+atoz = [chr(i) for i in range(97, 97 + 26)]
+input = sys.stdin.readline
+
+
+def main():
+    n = int(input())
+    s = list(map(int, input().split()))
+    t = list(map(int, input().split()))
+    st = []
+    for i in range(n):
+        st.append((s[i], t[i]))
+
+    ans, t = 0, 0
+    sorted(st, key=lambda ent: ent[1])
+
+    for i in range(n):
+        if t < st[i][0]:
+            ans += 1
+            t = st[i][1]
+
+    print(ans)
+    return
+
+
+if __name__ == '__main__':
+    main()
