d52c13f241c50d62f2caec8afacbb8665c637490
koucs <kou.cshu@gmail.com>
2021-12-05 12:36:16 +0900

    整理

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ### Test sample
 
 ```shell
-$ diff -Zu  <(python main.py < sample-1.in) <(cat ./sample-1.out)
+$ diff -Zu  <(python main.py < test-1.in) <(cat ./sample-1.out)
 --- /dev/fd/63  2021-12-05 08:34:58.818064233 +0900
 +++ /dev/fd/62  2021-12-05 08:34:58.818064233 +0900
 @@ -1 +1 @@
---
+++ ch2/2_3_最長共通部分列問題/main.py
@@ -0,0 +1,33 @@
+#! env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+# Ant-Book.main.py
+# Date: 2021/08/28
+# Filename: main.py 
+# Author: acttrd
+import pprint
+
+sys.setrecursionlimit(10 ** 8)
+INF = float('inf')
+mod = 10 ** 9 + 7
+eps = 10 ** -7
+AtoZ = [chr(i) for i in range(65, 65 + 26)]
+atoz = [chr(i) for i in range(97, 97 + 26)]
+input = sys.stdin.readline
+
+
+def main():
+    n = int(input())
+    m = int(input())
+    s = input()
+    l = input()
+
+    # dp = [[0] * (W+1) for _ in range(n+1)]
+
+    return
+
+
+if __name__ == '__main__':
+    main()
