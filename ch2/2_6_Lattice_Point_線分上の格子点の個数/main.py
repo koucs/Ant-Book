c15a5b7c3b1cfd0bd48e09ce2fd5516a3a362aa4
koucs <kou.cshu@gmail.com>
2022-01-02 17:50:20 +0900

    2_6 Sugoroku

--- ch2/2_6_Lattice_Point_線分上の格子点の個数/main.py
+++ ch2/2_6_Lattice_Point_線分上の格子点の個数/main.py
@@ -1,23 +1,11 @@
 #! env python
 # -*- coding: utf-8 -*-
 
-from collections import defaultdict
-import sys, heapq, bisect, math, itertools, string, queue, datetime, unittest
-
 # Ant-Book.main.py
 # Date: 2022/01/02
 # Filename: main.py 
 # Author: koucs
-# 格子点(lattice point)
-# = 整数の点
-
-sys.setrecursionlimit(10 ** 8)
-INF = float('inf')
-mod = 10 ** 9 + 7
-eps = 10 ** -7
-AtoZ = [chr(i) for i in range(65, 65 + 26)]
-atoz = [chr(i) for i in range(97, 97 + 26)]
-input = sys.stdin.readline
+# 格子点(lattice point) = 整数の点
 
 def gcd(x: int, y: int):
     if y == 0:
